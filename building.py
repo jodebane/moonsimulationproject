b02ca411f956c4c4351732eee5cd9a9df6485dca
Joseph Foley <josephfoley@rcn.com>
2019-08-31 15:47:24 -0400

    Update building.py
    building class. allows creation of buildings for the player's moon base

--- building.py
+++ building.py
@@ -9,6 +9,32 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-class building:
-    cost = int
+##building class
+class building():
+    def __init__(self, name, cost):
+        self.name = name
+        self.cost = cost
+        self.initfunction = []
+        self.perturnfunction = []
+#allows initial functions to be added to building objects
+    def addinitfunction(self, effectstring):
+        self.initfunction.append(effectstring)
+#allows per turn effects to be adedd to building objects
+    def addperturnfunction(self,effectstring):
+        self.perturnfunction.append(effectstring)
+    def printname(self):
+        print(self.name)
+    def printcost(self):
+        print(self.cost)
+    def printinitfunction(self):
+        print(self.initfunction)
+    def printperturnfunction(self):
+        print(self.perturnfunction)
+##    def printinfo(self)
+    def printall(self):
+        self.printname()
+        self.printcost()
+        self.printinitfunction()
+        self.printperturnfunction()
+
 
